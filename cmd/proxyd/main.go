ee67196309da1a85fcc827c16453e1a6df526ed7
ensonmj <ensonmj@gmail.com>
2020-04-16 14:26:17 +0800

    use go mod

--- cmd/proxyd/main.go
+++ cmd/proxyd/main.go
@@ -17,24 +17,28 @@ func main() {
 
 	pflag.Parse()
 
-	if *fVerbose < 0 || *fVerbose > 5 {
-		*fVerbose = 0
+	if *fVerbose < 0 || *fVerbose > 6 {
+		*fVerbose = 3
 	}
 	proxy.SetLevel(*fVerbose)
 
 	var wg sync.WaitGroup
 	for _, strNode := range *fLocalNodes {
-		srv, err := proxy.NewServer(strNode, *fChainNodes...)
-		if err != nil {
-			log.Printf("%+v\n", err)
-			continue
-		}
-
-		wg.Add(1)
-		go func(srv *proxy.Server) {
-			defer wg.Done()
-			log.Printf("%+v\n", srv.ListenAndServe())
-		}(srv)
+		asyncListenAndServe(&wg, strNode, *fChainNodes...)
 	}
 	wg.Wait()
+}
+
+func asyncListenAndServe(wg *sync.WaitGroup, localURL string, chainURL ...string) {
+	srv, err := proxy.NewServer(localURL, chainURL...)
+	if err != nil {
+		log.Printf("%+v\n", err)
+		return
+	}
+
+	wg.Add(1)
+	go func(srv *proxy.Server) {
+		defer wg.Done()
+		log.Printf("%+v\n", srv.ListenAndServe())
+	}(srv)
 }
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/ensonmj/proxy
+
+go 1.13
+
+require (
+	github.com/pkg/errors v0.8.1
+	github.com/sirupsen/logrus v1.4.2
+	github.com/spf13/pflag v1.0.3
+	golang.org/x/net v0.0.0-20190813141303-74dc4d7220e7
+)
